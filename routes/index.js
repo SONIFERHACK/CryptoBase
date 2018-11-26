b798c3558eef333f8995944cb557c5420522e20f
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-26 17:09:19 +0100

    The code has been added to import tickers and graph OHLCV data. Next Step will be to draw charts with Chartjs.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const ccxt = require('ccxt');
-// let bitfinex = new ccxt.bitfinex();
+
 
 
 
@@ -10,16 +10,40 @@ const ccxt = require('ccxt');
 /* GET home page */
 router.get('/', (req, res, next) => {
 
+
   (async () => {
-    let bitfinex = new ccxt.bitfinex ()
-    let markets = await bitfinex.load_markets ()
-    console.log (bitfinex.id, markets)
-  }) ()
+    let bitfinex = new ccxt.bitfinex()
+    let series = [];
+    let markets = await bitfinex.load_markets()
+
+    // console.log (bitfinex.id, markets)
+
+    // if (bitfinex.has['fetchTickers']) {
+    //   console.log(await (bitfinex.fetchTickers())) // all tickers indexed by their symbols
+    // }
 
 
-  // console.log(ccxt.exchanges);
-  // console.log(bitfinex.id + " " + bitfinex.name);
-  res.render('index');
+    // if (bitfinex.has['fetchTicker']) {
+      // console.log (await (bitfinex.fetchTicker ('BTC/USDT'))) // ticker for BTC/USD
+      // let symbols = Object.keys (exchange.markets)
+      // let random = Math.floor (Math.random () * (symbols.length - 1))
+      // console.log (exchange.fetchTicker (symbols[random])) // ticker for a random symbol
+  // }
+
+    let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    if (bitfinex.has.fetchOHLCV) {
+      // for (symbol in bitfinex.markets) {
+        await sleep(bitfinex.rateLimit) // milliseconds
+        // console.log(await bitfinex.fetchOHLCV('BTC/USDT', '1h', 1456843334)) // one hour
+      series = await bitfinex.fetchOHLCV('BTC/USDT', '1h', 1456843334)
+      console.log({series})
+      // }
+    }
+
+    await res.render('index', {series});
+  })()
+
+  
 });
 
 module.exports = router;
