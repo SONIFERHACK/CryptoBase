264b768b4cd868d29563fe96ea6480732f268eb7
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-26 13:40:59 +0100

    another step towards importing the tickers

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const ccxt = require('ccxt');
-let bitfinex = new ccxt.bitfinex();
+// let bitfinex = new ccxt.bitfinex();
+
+
 
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  console.log(ccxt.exchanges);
-  console.log(bitfinex);
+
+  (async () => {
+    let bitfinex = new ccxt.bitfinex ()
+    let markets = await bitfinex.load_markets ()
+    console.log (bitfinex.id, markets)
+  }) ()
+
+
+  // console.log(ccxt.exchanges);
+  // console.log(bitfinex.id + " " + bitfinex.name);
   res.render('index');
 });
 
