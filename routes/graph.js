7324daa49f46ecdbf5cd67e82978f3a6e698d749
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-27 17:22:37 +0100

    The chart now updates via an axios call, which uses an internal get route of our server to call the CCXT API.

--- routes/graph.js
+++ routes/graph.js
@@ -13,34 +13,44 @@ router.get('/graph', (req, res, next) => {
     let series = [];
     let markets = await bitfinex.load_markets()
 
-    // console.log (bitfinex.id, markets)
+    let inputSymbol = 'BTC/USDT', timeUnit = '5m', sinceTs = 1543017600000;
 
-    // if (bitfinex.has['fetchTickers']) {
-    //   console.log(await (bitfinex.fetchTickers())) // all tickers indexed by their symbols
-    // }
-
-
-    // if (bitfinex.has['fetchTicker']) {
-      // console.log (await (bitfinex.fetchTicker ('BTC/USDT'))) // ticker for BTC/USD
-      // let symbols = Object.keys (exchange.markets)
-      // let random = Math.floor (Math.random () * (symbols.length - 1))
-      // console.log (exchange.fetchTicker (symbols[random])) // ticker for a random symbol
-  // }
-    let inputSymbol = 'BTC/USDT', timeUnit = '1h', sinceTs = 1543017600000;
     let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    
     if (bitfinex.has.fetchOHLCV) {
-      // for (symbol in bitfinex.markets) {
-        await sleep(bitfinex.rateLimit) // milliseconds
-        // console.log(await bitfinex.fetchOHLCV('BTC/USDT', '1h', 1456843334)) // one hour 1456843334000
+      await sleep(bitfinex.rateLimit) // milliseconds
       series = await bitfinex.fetchOHLCV(inputSymbol, timeUnit, sinceTs);
-      // console.log({series})
-      // }
     }
 
-    await res.render('graph', {series});
+    await res.render('graph', { series });
   })()
 
-  
+
 });
+
+
+router.get('/seriesQuery', (req,res) => {
+
+  (async () => {
+    let bitfinex = new ccxt.bitfinex()
+    let series = [];
+    let markets = await bitfinex.load_markets()
+
+    let inputSymbol = 'BTC/USDT', timeUnit = '1m', sinceTs = 1543333818000;
+
+    let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    
+    if (bitfinex.has.fetchOHLCV) {
+      await sleep(bitfinex.rateLimit) // milliseconds
+      series = await bitfinex.fetchOHLCV(inputSymbol, timeUnit, sinceTs);
+    }
+    // console.log('sending json of: ' + series)
+    await res.json( series );
+  })()
+
+})
+
+
+
 
 module.exports = router;
