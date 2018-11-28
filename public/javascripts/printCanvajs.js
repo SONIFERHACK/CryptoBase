79d9bf65c8af7a1cbf9e9845041d7a6c4d4200d4
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-28 02:57:14 +0100

    Added support for graphs

---
+++ public/javascripts/printCanvajs.js
@@ -0,0 +1,80 @@
+window.onload = function () {
+
+  var dataPoints = [];
+
+  dataPoints = series.map((elem) => {
+    let ts = new Date(elem[0]);
+
+    //0 - timestamp, 1 - Open, 2 - High, 3 - Low, 4 - Close, 5 - Volume
+    return { x: ts, y: [elem[1], elem[2], elem[3], elem[4]] };
+  });
+
+  var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "light2", // "light1", "light2", "dark1", "dark2"
+    exportEnabled: true,
+    title: {
+      text: "BTC/USDT on Bitfinex"
+    },
+    axisX: {
+      valueFormatString: "DD HH:mm"
+    },
+    axisY: {
+      includeZero: false,
+      prefix: "$",
+      title: "Price (in USD)"
+    },
+    toolTip: {
+      shared: true
+    },
+    legend: {
+      cursor: "pointer",
+      itemclick: toogleDataSeries
+    },
+    data: [{
+      type: "candlestick",
+      showInLegend: true,
+      name: "BTC",
+      yValueFormatString: "$###0.00",
+      xValueFormatString: "MMM DD HH:mm",
+      dataPoints: dataPoints
+    },
+    ]
+  });
+
+
+  let refreshIntID = setInterval(function () {
+    console.log('entra tb');
+    axios.get('/seriesQuery')
+      .then((newSeries) => {
+
+        newDataPoints = newSeries.map((elem) => {
+          let ts = new Date(elem[0]);
+
+          //0 - timestamp, 1 - Open, 2 - High, 3 - Low, 4 - Close, 5 - Volume
+          return { x: ts, y: [elem[1], elem[2], elem[3], elem[4]] };
+        });
+
+        chart.data.datapoints = newDataPoints;
+        chart.render();
+      })
+      .catch((err) => {
+        return err
+      })
+
+  }, 30000);
+
+  setTimeout(function () {
+    clearInterval(refreshIntID)
+  }, 60000);
+
+  function toogleDataSeries(e) {
+    if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+      e.dataSeries.visible = false;
+    } else {
+      e.dataSeries.visible = true;
+    }
+    e.chart.render();
+  }
+
+}
---
+++ public/javascripts/printChartjs.js
@@ -0,0 +1,85 @@
+
+function printChart(series, refreshInterval) {
+
+  seriesTime = series.map((elem) => {
+    let ts = new Date(elem[0]);
+    return ts.toLocaleString()
+  })
+
+  let y1 = series.map((elem) => {
+    return elem[1]; //1 - Open, 2 - High, 3 - Low, 4 - Close, 5 - Volume
+  })
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+      labels: seriesTime,
+      datasets: [{
+        label: "My First dataset",
+        fill: false,
+        pointRadius: 0,
+        lineTension: 0,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: y1,
+      }]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+
+
+  let refreshIntID = setInterval(function () {
+    console.log('entra');
+    axios.get('/seriesQuery')
+      .then((newSeries) => {
+
+        newSeriesTime = newSeries.data.map((elem) => {
+          let ts = new Date(elem[0]);
+          return ts.toLocaleString()
+        })
+
+        let newY1 = newSeries.data.map((elem) => {
+          return elem[1]; //1 - Open, 2 - High, 3 - Low, 4 - Close, 5 - Volume
+        })
+
+        chart.data.labels = newSeriesTime;
+        chart.data.datasets.data = newY1;
+        chart.update();
+
+      })
+      .catch((err) => {
+        return err
+      })
+
+  }, 30000);
+
+  setTimeout(function () {
+    clearInterval(refreshIntID)
+  }, 60000);
+
+}
+
+printChart(series);
+
+
+
+
+
+// setTimeout(function(){
+//   console.log('entra');
+//   axios.get('/seriesQuery')
+//   .then((newSeries)=>{
+//     console.log(newSeries.data)
+//     printChart(newSeries.data)
+//   })
+//   .catch((err)=>{
+//     return err
+//   })
+
+// }, 5000)
