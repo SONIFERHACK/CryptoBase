5a832e98b20d7ce6cb556c330870c8aa58b0e94b
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-28 17:47:00 +0100

    Changes at the frontend

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const ensureLogin = require("connect-ensure-login");
     
 
 mongoose
-  .connect('mongodb://localhost/cryptobase', {useNewUrlParser: true})
+  .connect(process.env.DBURL, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database`)
   })
@@ -61,6 +61,7 @@ hbs.registerHelper('ifUndefined', (value, options) => {
 });
 
 hbs.registerHelper('json', function(context) {
+  
   return JSON.stringify(context);
 });
   
