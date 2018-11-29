6ffe083e2e592f48edebf6bdf85d9fadc0aabe1c
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-29 18:56:27 +0100

    added view and route for second graph

--- routes/graph.js
+++ routes/graph.js
@@ -5,7 +5,7 @@ const { ensureLoggedIn } = require("connect-ensure-login");
 const ccxt = require('ccxt');
 
 
-/* GET home page */
+
 router.get('/graph', ensureLoggedIn({redirectTo:'/auth/login'}),(req, res, next) => {
 
 
@@ -24,6 +24,29 @@ router.get('/graph', ensureLoggedIn({redirectTo:'/auth/login'}),(req, res, next)
     }
 
     await res.render('graph', { series });
+  })()
+
+
+});
+
+router.get('/graph2', ensureLoggedIn({redirectTo:'/auth/login'}),(req, res, next) => {
+
+
+  (async () => {
+    let bitfinex = new ccxt.bitfinex()
+    let series = [];
+    let markets = await bitfinex.load_markets()
+
+    let inputSymbol = 'BTC/USDT', timeUnit = '5m', sinceTs = 1543017600000;
+
+    let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    
+    if (bitfinex.has.fetchOHLCV) {
+      await sleep(bitfinex.rateLimit) // milliseconds
+      series = await bitfinex.fetchOHLCV(inputSymbol, timeUnit, sinceTs);
+    }
+
+    await res.render('graph2', { series });
   })()
 
 
