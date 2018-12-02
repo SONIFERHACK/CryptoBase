1ae0deceda77cccea462da11da1988d8de3c0040
Fer Perez Ayala <ferp@protonmail.ch>
2018-12-02 15:23:38 +0100

    Got the table to sort by 24hr Volume both ascending and descending by clicking on the column header. Brought back an image which was mistakenly removed from the repository.

--- routes/market.js
+++ routes/market.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 
 router.get("/market", (req, res, next) => {
 
-  axios.get("https://api.coinmarketcap.com/v2/ticker/?limit=100")
+  axios.get("https://api.coinmarketcap.com/v2/ticker/?limit=100&structure=array")
   .then((tickers2) => {
     console.log(tickers2);
     let tickers = tickers2.data.data;
